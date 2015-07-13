15368724ee2185a99a8f0111c94eed0520d06da8
dgcollier <david@gcollier.me>
2015-07-13 11:36:32 -0500

    Actually completed, with Step 3 included.

--- do_while.php
+++ do_while.php
@@ -1,17 +1,27 @@
 <?php
+// ITEM ONE
+// $a = 0;
 
-$a = 0;
+// do {
+//     echo $a . PHP_EOL;
+//     $a += 2;
+// } while ($a <= 100);
+
+// ITEM TWO
+// $a = 100;
+
+// do {
+//     echo $a . PHP_EOL;
+//     $a -= 5;
+// } while ($a >= -10);
+
+// ITEM THREE
+
+$a = 2;
 
 do {
     echo $a . PHP_EOL;
-    $a += 2;
-} while ($a <= 100);
-
-$a = 100;
-
-do {
-    echo $a . PHP_EOL;
-    $a -= 5;
-} while ($a >= -10);
+    $a *= $a;
+} while ($a <= 1000000);
 
 ?>
