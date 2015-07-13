b4e55a428dc0c4960cc42bdba7932963ab38bc21
dgcollier <david@gcollier.me>
2015-07-13 09:48:57 -0500

    Finished introduction to PHP

--- intro.php
+++ intro.php
@@ -5,4 +5,14 @@
     echo "Hello World" . PHP_EOL;
 
     echo "My name is $name" . PHP_EOL;
+
+    $a = 5;
+    $b = 10;
+    $c = '10';
+
+    if ($a == $b) {
+        echo "$a is equal to $b" . PHP_EOL;
+    } else {
+        echo "$a is not equal to $b" . PHP_EOL;
+    }
 ?>
