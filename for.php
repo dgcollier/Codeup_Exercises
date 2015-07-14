66130a20b208b220cd14eda0b1c7d8fd04d24aa3
dgcollier <david@gcollier.me>
2015-07-14 11:16:48 -0500

    Completed Step 3 of "FOR" exercise in PHP lesson. Increments based on user input. Defaults to 1 if user input is NaN or is 0.

--- for.php
+++ for.php
@@ -7,6 +7,10 @@ $endNum = intval(fgets(STDIN));
 fwrite(STDOUT, 'Enter an number to increment by:' . PHP_EOL);
 $increment = intval(fgets(STDIN));
 
+if ($increment == 0) {
+    $increment = 1;
+}
+
 for ($i = $startNum; $i <= $endNum; $i += $increment) {
     sleep(1);
     echo trim($i) . PHP_EOL;
