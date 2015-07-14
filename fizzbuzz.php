728fb00ee8e6e31570a1ca5050fbf9cf2e484673
dgcollier <david@gcollier.me>
2015-07-14 11:38:48 -0500

    Same exercise using while loop instead of for loop.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -4,16 +4,31 @@ But for multiples of three print “Fizz” instead of the number
 and for the multiples of five print “Buzz”. For numbers which 
 are multiples of both three and five print “FizzBuzz”. */
 
-for ($i = 1; $i <= 100; $i++) {
+// for ($i = 1; $i <= 100; $i++) {
+//     if ($i % 5 == 0 && $i % 3 == 0) {
+//         echo 'FizzBuzz' . PHP_EOL;
+//     } else if ($i % 5 == 0) {
+//         echo 'Buzz' . PHP_EOL;
+//     } else if ($i % 3 == 0) {
+//         echo 'Fizz' . PHP_EOL;
+//     } else {
+//         echo $i . PHP_EOL;
+//     }
+// }
+
+$i = 1;
+while ($i < 100) {
+    $i++;
+
     if ($i % 5 == 0 && $i % 3 == 0) {
-        echo 'FizzBuzz' . PHP_EOL;
-    } else if ($i % 5 == 0) {
-        echo 'Buzz' . PHP_EOL;
-    } else if ($i % 3 == 0) {
-        echo 'Fizz' . PHP_EOL;
-    } else {
-        echo $i . PHP_EOL;
-    }
+            echo 'FizzBuzz' . PHP_EOL;
+        } else if ($i % 5 == 0) {
+            echo 'Buzz' . PHP_EOL;
+        } else if ($i % 3 == 0) {
+            echo 'Fizz' . PHP_EOL;
+        } else {
+            echo $i . PHP_EOL;
+        }
 }
 
 ?>
