7159e7f18b63a0151fa9040e2f7823480d183980
dgcollier <david@gcollier.me>
2015-07-14 15:12:51 -0500

    Loops through each book and each book's keys and values. Outputs book's title and lists the key/value pairs for each book.

---
+++ foreach_books.php
@@ -0,0 +1,35 @@
+<?php
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+foreach ($books as $titles => $info) {
+        echo "{$titles}" . PHP_EOL;
+    foreach ($info as $attributes => $value) {
+        echo "{$attributes}: {$value}" . PHP_EOL;
+    }
+
+    echo ' ' . PHP_EOL;
+}
+
+?>
