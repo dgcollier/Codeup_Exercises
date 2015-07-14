26b68d4ccef846c718f46d8dec81364fec5b3efd
dgcollier <david@gcollier.me>
2015-07-14 11:26:59 -0500

    Completed Step  4 of "FOR" exercise in PHP lesson. Only allows users to input numbers for start and end, error prompts until user enters a number.

--- for.php
+++ for.php
@@ -1,9 +1,21 @@
 <?php
 
 fwrite(STDOUT, 'Enter start number:' . PHP_EOL);
-$startNum = intval(fgets(STDIN));
+$startNum = trim(fgets(STDIN));
+
+do { 
+    fwrite(STDOUT, 'Enter a NUMBER:' . PHP_EOL);
+    $startNum = trim(fgets(STDIN)); 
+} while (!is_numeric($startNum));
+
 fwrite(STDOUT, 'Enter end number:' . PHP_EOL);
-$endNum = intval(fgets(STDIN));
+$endNum = trim(fgets(STDIN));
+
+do {
+    fwrite(STDOUT, 'Enter a NUMBER:' . PHP_EOL);
+    $endNum = trim(fgets(STDIN)); 
+} while (!is_numeric($endNum));
+
 fwrite(STDOUT, 'Enter an number to increment by:' . PHP_EOL);
 $increment = intval(fgets(STDIN));
 
