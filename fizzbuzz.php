5bed50b09e2467d0cdbddd64cdc084783bec84b8
dgcollier <david@gcollier.me>
2015-07-14 11:35:31 -0500

    Completed exercise. Prints "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for multiples of both.

---
+++ fizzbuzz.php
@@ -0,0 +1,19 @@
+<?php
+/*Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number 
+and for the multiples of five print “Buzz”. For numbers which 
+are multiples of both three and five print “FizzBuzz”. */
+
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 5 == 0 && $i % 3 == 0) {
+        echo 'FizzBuzz' . PHP_EOL;
+    } else if ($i % 5 == 0) {
+        echo 'Buzz' . PHP_EOL;
+    } else if ($i % 3 == 0) {
+        echo 'Fizz' . PHP_EOL;
+    } else {
+        echo $i . PHP_EOL;
+    }
+}
+
+?>
