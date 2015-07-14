42a7ff0fa4698aafb465f433ec08f2f3e61ec5a0
dgcollier <david@gcollier.me>
2015-07-14 11:41:49 -0500

    Same exercise with do-while loop instead of while or for loops.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -4,6 +4,8 @@ But for multiples of three print “Fizz” instead of the number
 and for the multiples of five print “Buzz”. For numbers which 
 are multiples of both three and five print “FizzBuzz”. */
 
+// FOR LOOP EXAMPLE
+
 // for ($i = 1; $i <= 100; $i++) {
 //     if ($i % 5 == 0 && $i % 3 == 0) {
 //         echo 'FizzBuzz' . PHP_EOL;
@@ -16,10 +18,27 @@ are multiples of both three and five print “FizzBuzz”. */
 //     }
 // }
 
-$i = 1;
-while ($i < 100) {
-    $i++;
+// WHILE LOOP EXAMPLE
 
+// $i = 1;
+// while ($i < 100) {
+//     $i++;
+
+//     if ($i % 5 == 0 && $i % 3 == 0) {
+//             echo 'FizzBuzz' . PHP_EOL;
+//         } else if ($i % 5 == 0) {
+//             echo 'Buzz' . PHP_EOL;
+//         } else if ($i % 3 == 0) {
+//             echo 'Fizz' . PHP_EOL;
+//         } else {
+//             echo $i . PHP_EOL;
+//         }
+// }
+
+// DO WHILE LOOP EXAMPLE
+
+$i = 1;
+do {
     if ($i % 5 == 0 && $i % 3 == 0) {
             echo 'FizzBuzz' . PHP_EOL;
         } else if ($i % 5 == 0) {
@@ -29,6 +48,8 @@ while ($i < 100) {
         } else {
             echo $i . PHP_EOL;
         }
-}
+    $i++;
+
+} while ($i <= 100)
 
 ?>
