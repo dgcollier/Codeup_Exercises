32ac80f56394db7a1e0fb10cd67440aadc954eb9
dgcollier <david@gcollier.me>
2015-07-15 13:39:00 -0500

    Moved checks for arguments into own function. Checks if dividing then if by zero. If not dividing, checks for numeric arguments. Tested error messages with success.

--- arithmetic.php
+++ arithmetic.php
@@ -1,53 +1,47 @@
 <?php
 
-$a = 'hi';
-$b = 0;
+$a = 10;
+$b = 2;
+
+function checkArguments($a, $b, $dividing = false) {
+    if (is_numeric($a) && is_numeric($b)) {
+        if ($dividing && $b == 0) {
+            echo 'ERROR: Cannot divide by 0!' . PHP_EOL;
+            return false;
+        }
+        return true;
+    } else {
+        echo "ERROR: Both '{$a}' and '{$b}' must be numbers." . PHP_EOL;
+    }
+}
 
 function add($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (checkArguments($a, $b)) {
         return $a + $b . PHP_EOL;
-    } else {
-        return "ERROR: Both arguments must be numbers. You tried to pass '{$a}' and '{$b}'." . PHP_EOL;
     }
 }
 
 function subtract($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (checkArguments($a, $b)) {
         return $a - $b . PHP_EOL;
-    } else {
-        return "ERROR: Both arguments must be numbers. You tried to pass '{$a}' and '{$b}'." . PHP_EOL;
     }
 }
 
 function multiply($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (checkArguments($a, $b)) {
         return $a * $b . PHP_EOL;
-    } else {
-        return "ERROR: Both arguments must be numbers. You tried to pass '{$a}' and '{$b}'." . PHP_EOL;
     }
 }
 
 function divide($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
-        if ($b == 0) {
-            echo 'ERROR: Cannot divide by 0!' . PHP_EOL;
-        } else {
+    if (checkArguments($a, $b, true)) {
             return $a / $b . PHP_EOL;
-        }
-    } else {
-        return "ERROR: Both arguments must be numbers. You tried to pass '{$a}' and '{$b}'." . PHP_EOL;
     }
 }
 
 function modulus($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
-        if ($b == 0) {
-            echo 'ERROR: Cannot divide by 0!' . PHP_EOL;
-        } else {
+    if (checkArguments($a, $b, true)) {
             return $a % $b . PHP_EOL;
-        }
-    } else {
-        return "ERROR: Both arguments must be numbers. You tried to pass '{$a}' and '{$b}'." . PHP_EOL;
     }
 }
 
