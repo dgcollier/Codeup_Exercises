31fe3da82fc49c474d1365e414ca70822aaa7da8
dgcollier <david@gcollier.me>
2015-07-15 11:32:29 -0500

    Created variables and called each function passing those variables. Changed variables and tested output successfully.

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,8 @@
 <?php
 
+$a = 3;
+$b = 10;
+
 function add($a, $b)
 {
     return $a + $b . PHP_EOL;
@@ -26,8 +29,16 @@ function modulus($a, $b)
 }
 
 // Add code to test your functions here
+echo 'Passing numbers:' . PHP_EOL;
 echo add(5, 10);
 echo subtract(10, 5);
 echo multiply(10, 5);
 echo divide(10, 5);
 echo modulus(10, 2);
+
+echo 'Passing global variables:' . PHP_EOL;
+echo add($a, $b);
+echo subtract($a, $b);
+echo multiply($a, $b);
+echo divide($a, $b);
+echo modulus($a, $b);
