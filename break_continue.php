7feaf4cbdc915f11e168bcacd1d68304f05255a8
dgcollier <david@gcollier.me>
2015-07-15 09:57:55 -0500

    Completed step 1 with continue.

--- break_continue.php
+++ break_continue.php
@@ -1,14 +1,19 @@
 <?php  
 
 // Create a for loop that shows all even numbers between 1 and 100 using continue.
-for () {
-
+for ($i = 1; $i < 100; $i++) {
+    if ($i % 2 != 0) {
+        continue;
+    } else {
+        echo $i . PHP_EOL;
+    }
 }
+
 
 // Create a for loop that counts from 1 to 100 but stops after 10 using break.
 
-for () {
-    
-}
+// for () {
+
+// }
 
 ?>
