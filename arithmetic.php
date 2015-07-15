99d25f56bd203d4654ef2dc03cb3286e0e8f605c
dgcollier <david@gcollier.me>
2015-07-15 11:24:16 -0500

    Added code to call each function and verified output.

--- arithmetic.php
+++ arithmetic.php
@@ -2,25 +2,29 @@
 
 function add($a, $b)
 {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b)
 {
     // Add code here
-    echo $a - $b;
+    echo $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b)
 {
     // Add code here
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b)
 {
     // Add code here
-    echo $a / $b;
+    echo $a / $b . PHP_EOL;
 }
 
 // Add code to test your functions here
+add(5, 10);
+subtract(10, 5);
+multiply(10, 5);
+divide(10, 5);
