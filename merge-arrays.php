091a6d3828b136c006ca21b61ac6625dc382bfe3
dgcollier <david@gcollier.me>
2015-07-16 15:14:53 -0500

    Completed assignment.

---
+++ merge-arrays.php
@@ -0,0 +1,25 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function combineArrays($array1, $array2) {
+    $combined = [];
+
+    foreach ($array1 as $key => $value) {
+
+        if ($array1[$key] == $array2[$key]) {
+            array_push($combined, $array1[$key]);
+        } else {
+            array_push($combined, $array1[$key]);
+            array_push($combined, $array2[$key]);
+        }
+    }
+
+   return $combined; 
+}
+
+print_r(combineArrays($names, $compare));
+
+?>
