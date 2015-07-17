758cc3427aa776bbc3f12eb61d645def81d55b48
dgcollier <david@gcollier.me>
2015-07-17 09:50:25 -0500

    Returns specified solution within parameters of exercise. Step one complete.
    Need to 1) order alphabetically 2) make sorting optional 3) add default argument for sorting.

---
+++ list_with_commas.php
@@ -0,0 +1,28 @@
+<?php  
+
+// Converts array into list n1, n2, ..., and n3
+
+function humanizedList($array) {
+// Your solution goes here!
+    $arrayPop = array_pop($array);
+    array_push($array, "and $arrayPop");
+
+    $humanizedString = implode(', ', $array);
+
+    return $humanizedString;
+
+}
+
+// List of famous peeps
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+// Converts the string into an array
+$physicistsArray = explode(', ', $physicistsString);
+
+// Humanize that list
+$famousFakePhysicists = humanizedList($physicistsArray);
+
+// Output sentence
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+
+?>
