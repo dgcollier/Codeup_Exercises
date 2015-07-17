8223b543ff7091eb8995faeb14a1964ddb54052a
dgcollier <david@gcollier.me>
2015-07-17 14:40:51 -0500

    Reformatted array definition.

--- contact-parser.php
+++ contact-parser.php
@@ -16,7 +16,10 @@ function parseContacts($filename)
 
     foreach ($new_contacts as $key => $value) {
         $number = substr($value[1], 0, 3) . '-' . substr($value[1], 3, 3) . '-' . substr($value[1], 6, 4);
-        $new_contacts[$key] = array("name" => $value[0], "number" => $number);
+        $new_contacts[$key] = array(
+            "name" => $value[0], 
+            "number" => $number
+        );
     }
 
     return $new_contacts;
