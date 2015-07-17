225cc1cf42af7c5a105033530f6c138b4ee3c324
dgcollier <david@gcollier.me>
2015-07-17 13:58:36 -0500

    Completed in-class exercise.

--- contact-parser.php
+++ contact-parser.php
@@ -3,10 +3,49 @@
 function parseContacts($filename)
 {
     $contacts = array();
+    $new_contacts = [];
 
     // todo - read file and parse contacts
+
+    $handle = fopen($filename, 'r');
+    $contacts = fread($handle, filesize($filename));
+    fclose($handle);
+
+    $contacts = explode(PHP_EOL, $contacts);
+    // print_r($contacts);
+    // $contacts = implode('|', $contacts);
+    // print_r($contacts);
+    // $contacts = explode('|', $contacts);
+
+    foreach ($contacts as $contact) {
+        array_push($new_contacts, explode('|', $contact));
+    }
+
+    foreach ($new_contacts as $key => $value) {
+        $number = substr($value[1], 0, 3) . '-' . substr($value[1], 3, 3) . '-' . substr($value[1], 6, 4);
+        $new_contacts[$key] = array("name" => $value[0], "number" => $number);
+    }
+
+    $contacts = $new_contacts;
+
+    // foreach ($contacts as $contact) {
+    //     foreach ($contact as $key => $value) {
+
+    //         $i = intval($key);
+    //         if ($i % 2 == 0) {
+    //             $key = ["name"];
+    //             // var_dump($contacts);
+    //             unset($key);
+    //         } else {
+    //             $key = ["number"];
+    //             unset($key); 
+    //         }
+    //     }
+    // }
 
     return $contacts;
 }
 
-var_dump(parseContacts('contacts.txt'));
+print_r(parseContacts('contacts.txt'));
+
+?>
