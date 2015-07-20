c66a2664f4c5e095e93bbbc6ac2d4d3311a2da7a
dgcollier <david@gcollier.me>
2015-07-20 10:17:10 -0500

    Completed in-class exercise. Basic logger created for info and error messages.

---
+++ logger.php
@@ -0,0 +1,28 @@
+<?php
+
+function logMessage($logLevel, $message)
+{
+    // todo - complete this function
+    date_default_timezone_set('America/Chicago');
+    $date = date('Y-m-d');
+    $time = date('h:i:s');
+    $handle = fopen('txt/log' . $date . '.txt', 'a');
+    $logLevel = strtoupper($logLevel);
+    $message = strtoupper($message);
+    fwrite($handle, "$date $time [$logLevel] $message" . PHP_EOL);
+    fclose($handle);
+    return print_r("Messaged logged." . PHP_EOL);
+}
+
+function logInfo ($infoMessage) 
+{
+    logMessage("INFO", $infoMessage);
+}
+
+function logError ($errorMessage) 
+{
+    logMessage("ERROR", $errorMessage);
+}
+
+logInfo("Today is Monday.");
+logError("That is also the error message.");
